63a308b84b84cf52f39a6b0753a67a77c8ef50ae
DESKTOP-9U8F004\ddrgis <kasabiankpp@gmail.com>
2017-05-08 14:49:47 +0200

    Added exercise 1.1.24

--- BasicAlgorithms/BasicAlgoritms.cs
+++ BasicAlgorithms/BasicAlgoritms.cs
@@ -15,6 +15,18 @@ namespace BasicAlgorithms
             return GCDRecursion(n2, reminder);
         }
 
+        public static int GCDRecursionTraced(int n1, int n2)
+        {
+            Console.WriteLine($"n1 = {n1}, n2 = {n2}");
+            if (n1 < 0 || n2 < 0)
+                throw new ArgumentException();
+            if (n2 == 0)
+                return n1;
+
+            int reminder = n1 % n2;
+            return GCDRecursionTraced(n2, reminder);
+        }
+
         public static int GCD(int n1, int n2)
         {
             if (n1 < 0 || n2 < 0)
---
+++ Ex_1_1_24/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex_1_1_24
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int result = BasicAlgorithms.BasicAlgoritms.GCDRecursionTraced(105, 24);
+            Console.WriteLine($"Done, result = {result}");
+            Console.WriteLine();
+            result = BasicAlgorithms.BasicAlgoritms.GCDRecursionTraced(1111111, 1234567);
+            Console.WriteLine($"Done, result = {result}");
+            Console.Read();
+        }
+    }
+}
